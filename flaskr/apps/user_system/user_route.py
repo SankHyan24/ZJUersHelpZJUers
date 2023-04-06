40d659f692a9aaa2ea966b7d14a5800ddd5f71d2
timako <72556626+timako@users.noreply.github.com>
2023-04-06 14:44:53 +0800

    User register(uncompleted)

--- flaskr/apps/user_system/user_route.py
+++ flaskr/apps/user_system/user_route.py
@@ -5,7 +5,7 @@ from flask import render_template, request, url_for, redirect, session
 
 @route("/info/modify",method=["GET","POST"])
 def info_modify():
-    if request.method = 'GET'
+    if request.method == 'GET':
         form_modify = InfoModifyForm()
         return render_template('',form_modify=form_modify)
     else:
@@ -17,3 +17,18 @@ def info_modify():
             print(form.errors, "Error Message")
 
             return render_template("info.html",info_form=form_modify)
+        
+@route("/register",method=["GET","POST"])
+def info_register():
+    if request.method == 'GET':
+        form = RegistrationForm()
+        return render_template('',form_register=form_register) # TODO
+
+    if request.method == 'POST':
+        form = RegistrationForm()
+        if form.validate():
+            info_register(session['user_id'], form.data)
+            return redirect(url_for('auth.login'))
+        else:
+            print(form.errors, "Error Message")
+            return render_template('',form_register=form_register) # TODO
