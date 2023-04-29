9ec839515ca5331435832f9d6766f11658613ba1
RaidenYing <hurryBoyha@gmail.com>
2023-04-29 14:42:17 +0800

    bp

--- flaskr/apps/user_system/user_route.py
+++ flaskr/apps/user_system/user_route.py
@@ -3,7 +3,9 @@ from flaskr.apps.user_system.user_model import *
 from flaskr.apps.user_system.user_form import *
 from flask import render_template, request, url_for, redirect, session
 
-@app.route("/info/modify",method=["GET","POST"])
+bp = Blueprint('user', __name__, url_prefix='/user')
+
+@bp.route("/info/modify",method=["GET","POST"])
 def info_modify():
     if request.method == 'GET':
         form_modify = InfoModifyForm()
@@ -18,17 +20,17 @@ def info_modify():
 
             return render_template("info.html",info_form=form_modify)
         
-@route("/register",method=["GET","POST"])
+@bp.route("/register",method=["GET","POST"])
 def info_register():
     if request.method == 'GET':
-        form = RegistrationForm()
+        form_register = RegistrationForm()
         return render_template('',form_register=form_register) # TODO
 
     if request.method == 'POST':
-        form = RegistrationForm()
-        if form.validate():
-            info_register(session['user_id'], form.data)
+        form_resgister = RegistrationForm()
+        if form_resgister.validate():
+            info_register(session['user_id'], form_resgister.data)
             return redirect(url_for('auth.login'))
         else:
-            print(form.errors, "Error Message")
+            print(form_resgister.errors, "Error Message")
             return render_template('',form_register=form_register) # TODO
