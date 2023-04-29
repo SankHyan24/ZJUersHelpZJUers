dda718dde9bb1103e85d8cb163ebe5990e17e5bb
scitbb <2209339564@qq.com>
2023-04-29 16:15:23 +0800

    modify order form

--- flaskr/apps/order_system/order_route.py
+++ flaskr/apps/order_system/order_route.py
@@ -1,34 +1,21 @@
-from __init__ import app
-from flaskr.apps.user_system.user_model import *
-from flaskr.apps.user_system.user_form import *
+from flaskr.apps.order_system.order_model import *
+from flaskr.apps.order_system.order_form import *
 from flask import render_template, request, url_for, redirect, session
 
-@app.route("/info/modify",method=["GET","POST"])
-def info_modify():
-    if request.method == 'GET':
-        form_modify = InfoModifyForm()
-        return render_template('',form_modify=form_modify)
-    else:
-        form_modify = InfoModifyForm(formdata=request.form)
-        if form_modify.validate():
-            infoModify(session['user_id'], form_modify.data)
-            return redirect(url_for('info'))
-        else:
-            print(form_modify.errors, "Error Message")
+bp = Blueprint('user', __name__, url_prefix='/order')
 
-            return render_template("info.html",info_form=form_modify)
         
-@route("/register",method=["GET","POST"])
-def info_register():
+@bp.route("/create_order",methods=["GET","POST"])
+def create_order():
     if request.method == 'GET':
-        form = RegistrationForm()
-        return render_template('',form_register=form_register) # TODO
-
-    if request.method == 'POST':
-        form = RegistrationForm()
-        if form.validate():
-            info_register(session['user_id'], form.data)
-            return redirect(url_for('auth.login'))
+        form_create_order = OrderInfoForm()
+        return render_template('',form_create_order=form_create_order)
+    else:
+        form_create_order = OrderInfoForm(formdata=request.form)
+        if form_create_order.validate():
+            order_create(session['user_id'], form_create_order.data)
+            return redirect(url_for('')) # TODO: redirect url
         else:
-            print(form.errors, "Error Message")
-            return render_template('',form_register=form_register) # TODO
+            print(form_create_order.errors, "Error Message")
+
+            return render_template("",info_form=form_create_order) # TODO: render html template
