60177c187c7704a6f0e963f77f65db80109b87d2
RaidenYing <hurryBoyha@gmail.com>
2023-04-29 17:26:20 +0800

    为主界面返回订单信息

--- flaskr/apps/order_system/order_route.py
+++ flaskr/apps/order_system/order_route.py
@@ -19,3 +19,13 @@ def create_order():
             print(form_create_order.errors, "Error Message")
 
             return render_template("",info_form=form_create_order) # TODO: render html template
+
+@bp.route("index.html",methods=["GET","POST"])
+def index():
+    newOID = newID()
+    for OID in newOID:
+        g.order[OID] = getFormData(OID)
+        g.newID = newOID
+    return redirect("index.html")
+    
+    
